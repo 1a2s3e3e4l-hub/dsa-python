2dc2e3d727992a74faef063ab24d35841ff9b3db
1a2s3e3e4l-hub <aseelsaleh11@icloud.com>
2026-02-24 23:41:20 +0200

    implement doubly linked list

--- linked_lists/doubly_linked_list.py
+++ linked_lists/doubly_linked_list.py
@@ -15,4 +15,15 @@ class DoublyLinkedList (Generic[T]):
     def __len__(self) -> int:
         return self.size
 
-
+    def insert_at_head(self,data:T) ->None:
+        new_node = DoublyNode(data)
+        new_node.data = data
+        if self.size == 0:
+            self.head=self.tail = new_node
+            new_node.next=new_node.prev=None
+        else:
+            new_node.next = self.head
+            new_node.prev = None
+            self.head.prev = new_node
+            self.head= new_node
+        self.size +=1
