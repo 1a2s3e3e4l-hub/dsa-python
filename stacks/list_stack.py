ebb3d1ccfdf19155ba8a416c851885784d33a299
1a2s3e3e4l-hub <aseelsaleh11@icloud.com>
2026-02-24 20:58:12 +0200

    Stacks: Implement list stack

--- stacks/list_stack.py
+++ stacks/list_stack.py
@@ -2,14 +2,20 @@ from typing import TypeVar, Generic ,List
 T = TypeVar ('T')
 class ListStack(Generic[T]):
     def __init__(self) -> None:
-
         self.items: List[T] = []
     
     def size(self) -> int:
         return len(self.items)
     
+    def is_empty(self)->bool:
+        return len(self.items) == 0
+    
     def push(self,item :T) -> None:
-        self.items.append(self.items)
+        self.items.append (item)
 
+    def pop(self) -> T:
+        if self.is_empty():
+            raise IndexError("pop from Empty Stack ")
+        return self.items.pop()
 
    
